de6a0b4cec69eaed2e457d9844af34d424852766
lky3004me <63594534+lky3004me@users.noreply.github.com>
2024-03-25 16:00:33 +0900

    Delete lky_subtree directory

--- lky_subtree/src/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("lky connection test");
-        System.out.println("testing");
-    }
-}
