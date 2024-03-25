755b3546c596f92b90d06c284099dda9e96777d0
lky3004me <lky3004me@gmail.com>
2024-03-25 15:36:26 +0900

    서브 트리 생성

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("lky connection test");
+        System.out.println("서브 트리 생성. 개인적으로 구조 생성 예정");
     }
 }
